7c609523dd443c2292a7dc0eb7893e8095f77a22
Nia Laureano <nialaureano@icloud.com>
2019-10-21 02:31:52 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,21 +3,21 @@ return (num1 + num2)
 end
 
 def subtraction(num1, num2)
-puts (num1 - num2)
+return (num1 - num2)
 end
 
 def division(num1, num2)
-puts (num1 / num2)
+return (num1 / num2)
 end
 
 def multiplication(num1, num2)
-puts (num1 * num2)
+return (num1 * num2)
 end
 
 def modulo(num1, num2)
-puts (num1 % num2)
+return (num1 % num2)
 end
 
 def square_root(num)
-puts Math.sqrt(num)
+return Math.sqrt(num)
 end
