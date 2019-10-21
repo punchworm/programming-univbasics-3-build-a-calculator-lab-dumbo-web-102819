2ca0f38c617fc6b079c0b6f965a50b615fb161ed
Nia Laureano <nialaureano@icloud.com>
2019-10-21 02:27:52 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,23 @@
 def addition(num1, num2)
-
+puts (num1.to_i + num2.to_i)
 end
 
 def subtraction(num1, num2)
-
+puts (num1.to_i - num2.to_i)
 end
 
 def division(num1, num2)
-
+puts (num1.to_i / num2.to_i)
 end
 
 def multiplication(num1, num2)
-
+puts (num1.to_i * num2.to_i)
 end
 
 def modulo(num1, num2)
-
+puts (num1.to_i % num2.to_i)
 end
 
 def square_root(num)
-
+puts Math.sqrt(num)
 end
